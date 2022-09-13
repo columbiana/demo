62ebef047e68c3a2ae08fdc5d4f3e225ebefdd07
columbiana <marko.trpeski@gmail.com>
2022-09-13 11:13:11 +0000

    Update server.js file

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 //App
 const app = express();
 app.get('/',(req,res) => {
-  res.send('Hello World testing!');
+  res.send('Git Actions working!');
 });
 
 app.listen(PORT,HOST);
